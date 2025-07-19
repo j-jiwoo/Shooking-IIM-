705b6514c1f26b22085b8a0765ea877f3830d858
정지우 <134187981+j-jiwoo@users.noreply.github.com>
2025-07-19 23:51:41 +0900

    make item cards

--- shooking/src/App.js
+++ shooking/src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  //상품 목록 데이터
+  const shoes = [{
+      image: 'https://via.placeholder.com/150?text=브랜드A',
+      brand: '브랜드A',
+      description: '편하고 착용감이 좋은 신발',
+      price: 35000
+    },
+    {
+      image: 'https://via.placeholder.com/150?text=브랜드A',
+      brand: '브랜드A',
+      description: '힙한 컬러가 매력적인 신발',
+      price: 25000
+    },
+    {
+      image: 'https://via.placeholder.com/150?text=브랜드B',
+      brand: '브랜드B',
+      description: '편안하고 착용감이 좋은 신발',
+      price: 35000
+    },
+    {
+      image: 'https://via.placeholder.com/150?text=브랜드B',
+      brand: '브랜드B',
+      description: '힙한 컬러가 매력적인 신발',
+      price: 35000
+    },
+    {
+      image: 'https://via.placeholder.com/150?text=브랜드C',
+      brand: '브랜드C',
+      description: '편안하고 착용감이 좋은 신발',
+      price: 35000
+    },
+    {
+      image: 'https://via.placeholder.com/150?text=브랜드C',
+      brand: '브랜드C',
+      description: '힙한 컬러가 매력적인 신발',
+      price: 35000
+    }
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='app-header'>
+        <div className="cart-icon">🛒</div>
       </header>
+      <h1>신발 상품 목록</h1>
+      <p>현재 {shoes.length}개의 상품이 있습니다.</p>
+
+      <div className='card-container'>
+        {shoes.map((shoe, index) => (
+          <div key={index} className='card'>
+            <img src={shoe.image} alt={shoe.brand}/>
+            <h3>{shoe.brand}</h3>
+            <p>{shoe.description}</p>
+            <p>{shoe.price}원</p>
+            <button>담기</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
